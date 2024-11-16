4eee3fa96827dbe0f9a308df95fcbf12861453ad
Samuel_Arruda <samuel.a.alves10@gmail.com>
2024-11-16 15:18:51 -0300

    conclusao do projeto

--- DesafioControleFluxo/src/Contar.java
+++ DesafioControleFluxo/src/Contar.java
@@ -2,30 +2,28 @@ import java.util.Scanner;
 
 public class Contar{
     public static void main(String[] args){
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro: ");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro: ");
-        int parametroDois = terminal.nextInt();
-
-        contagem(parametroUm, parametroDois);
-    }     
+        try {
+            Scanner terminal = new Scanner(System.in);
+            System.out.println("Digite o primeiro parâmetro: ");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro: ");
+            int parametroDois = terminal.nextInt();
+            terminal.close();
     
-    public static void contagem(int parametroUm, int parametroDois){
-        int contagem = parametroDois - parametroUm;
-        System.out.println("A diferença entre os dois parâmetros é " + contagem);
-
-        for(int contando = parametroUm; contando <= parametroDois; contando++) {
-            System.out.println("Imprimindo o número "+contando);
-        }
-        
-        /*while(numeroAtual <= parametroDois){
-            System.out.println("Contando: " + numeroAtual);
-            numeroAtual++;
-        }*/
-
-        if(parametroUm > parametroDois){
+            contagem(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
             System.out.println("O segundo número deve ser maior que o primeiro!");
         }
+    } 
+    public static void contagem(int parametroUm, int parametroDois) throws ParametrosInvalidosException{
+        int contaParametros = parametroDois - parametroUm;
+        
+            if(parametroUm >= parametroDois){
+            throw new ParametrosInvalidosException();
+            }
+
+            for(int contando = parametroUm; contando <= parametroDois; contando++) {
+            System.out.println("Imprimindo o número "+contando);
+            }
+        }
     }
-}
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,3 +1,5 @@
-/*class ParametrosInvalidosException extends Exception {
-
-}*/
+public class ParametrosInvalidosException extends Exception {
+    static String parametroExcecao(String contar) throws ParametrosInvalidosException{
+        return "O segundo número deve ser maior que o primeiro!";
+    }
+}
